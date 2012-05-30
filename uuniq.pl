6d3cfb49a5774d038567aa5b411fc09dec214a70
Urs Thuermann <urs@isnogud.escape.de>
2012-05-30 12:43:00 +0200

    Add option -i and usage message

--- uuniq.pl
+++ uuniq.pl
@@ -1,14 +1,25 @@
 #!/usr/bin/perl
 #
-# $Id: uuniq.pl,v 1.1 2012/05/30 10:42:46 urs Exp $
+# $Id: uuniq.pl,v 1.2 2012/05/30 10:43:00 urs Exp $
 #
 # Copy input lines to output removing duplicate lines.  In contrast to
 # the standard uniq(1) utility, duplicate lines do not need to be
 # adjacent to be removed.
 
+use Getopt::Std;
+
+if (!getopts("i")) {
+    die "Usage: $0 [-i] [file...]\n";
+}
+
 while (<>) {
-    if (!$seen{$_}) {
-	$seen{$_} = 1;
+    if (defined($opt_i)) {
+	($key = $_) =~ tr [A-Z] [a-z];
+    } else {
+	$key = $_;
+    }
+    if (!$seen{$key}) {
+	$seen{$key} = 1;
 	print;
     }
 }

df5c707c5b9a88c9c56223e58a48f4db6c23c06b
Urs Thuermann <urs@isnogud.escape.de>
2012-09-28 00:04:12 +0200

    Fix a stupid typo

--- uuniq.pl
+++ uuniq.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# $Id: uuniq.pl,v 1.3 2012/09/16 05:09:51 urs Exp $
+# $Id: uuniq.pl,v 1.4 2012/09/27 22:04:12 urs Exp $
 #
 # Copy input lines to output removing duplicate lines.  In contrast to
 # the standard uniq(1) utility, duplicate lines do not need to be
@@ -13,7 +13,7 @@ if (!getopts("i")) {
 }
 
 while (<>) {
-    $key = defined($opt_i) ? $key = lc($_) : $key = $_;
+    $key = defined($opt_i) ? lc($_) : $_;
     if (!$seen{$key}) {
 	$seen{$key} = 1;
 	print;
